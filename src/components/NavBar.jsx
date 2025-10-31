238416b72e351c7b23d7b5fc6417750e9fb56eb4
Ilyes Rouibi <ilyes.rouibi@epfl.ch>
2025-10-31 23:36:19 +0100

    feat: implement NavBar component with logo and navigation links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,20 @@
-function NavBar() {}
+import logo from "../assets/cloud-sun-alt-svgrepo-com.svg";
+function NavBar() {
+  return (
+    <header className="bg-red-500 p-3">
+      <div className="flex justify-around">
+        <a href="#home">
+          <img src={logo} className="h-8 w-8" />
+        </a>
+        <nav className="flex space-x-6">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
 
 export default NavBar;
