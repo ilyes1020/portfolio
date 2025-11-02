b77c2e9e7d6318f99776d701eb1d8ed7221e577d
Ilyes Rouibi <ilyes.rouibi@epfl.ch>
2025-11-02 01:33:52 +0100

    feat: refactor NavBar component to use Radix UI and improve layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,35 @@
-import logo from "../assets/cloud-sun-alt-svgrepo-com.svg";
+import { Flex, Link, Text } from "@radix-ui/themes";
+import { Camera } from "lucide-react";
+
 function NavBar() {
   return (
-    <header className="sticky top-0 bg-red-500 p-3">
-      <div className="flex justify-around">
-        <a href="#home">
-          <img src={logo} className="h-8 w-8" />
-        </a>
-        <nav className="flex space-x-6">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
-        </nav>
-      </div>
+    <header>
+      <Flex
+        position="sticky"
+        top="0"
+        justify="between"
+        align="center"
+        px="6"
+        py="3"
+      >
+        <Link href="#home" size="3">
+          <Camera />
+        </Link>
+        <Flex gap="4">
+          <Link href="#home" size="3">
+            <Text>Home</Text>
+          </Link>
+          <Link href="#about" size="3">
+            <Text>About</Text>
+          </Link>
+          <Link href="#projects" size="3">
+            <Text>Projects</Text>
+          </Link>
+          <Link href="#contact" size="3">
+            <Text>Contact</Text>
+          </Link>
+        </Flex>
+      </Flex>
     </header>
   );
 }
